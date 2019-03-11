204915266e6dc26da2482b867e34263a4a35d2b7
Andriy <skyba0andriy@gmail.com>
2019-03-11 19:47:02 +0200

    add CRUD for user_answer

--- queze/Tests/views.py
+++ queze/Tests/views.py
@@ -4,7 +4,7 @@ from django.forms.models import model_to_dict
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_http_methods
 
-from Tests.models import Test, Question, Answer, Results
+from Tests.models import Test, Question, Answer, Results, UserAnswer
 
 
 @require_http_methods(["POST"])
@@ -142,3 +142,35 @@ def get_results(request, test_id):
     except Exception:
         return HttpResponse(status=404)
     return JsonResponse(model_to_dict(result))
+
+@require_http_methods(["POST"])
+def create_user_answer(request):
+    data = json.loads(request.body)
+    user = request.user
+    test = request.test
+    user_answer = UserAnswer.create_quest(data["question"], user, test)
+    if user_answer:
+        return HttpResponse(status=201)
+    return  HttpResponse(status=404)
+
+@require_http_methods(["GET"])
+def get_user_answer(request, user_answer_id):
+    try:
+        user_answer = UserAnswer.object.get(user_answer_id)
+    except Exception:
+        return HttpResponse(status=404)
+    return JsonResponse(model_to_dict(user_answer))
+
+@require_http_methods(["PUT"])
+def update_user_answer(request, user_answer_id):
+    try:
+        user_answer = UserAnswer.object.get(id=user_answer_id)
+    except Exception:
+        return HttpResponse(status=404)
+    data = json.loads(request.body)
+    user_answer.question = data["question"]
+    try:
+        user_answer.save()
+        return JsonResponse({'message': 'Answer was successfully updated.'}, status=204)
+    except Exception:
+        return HttpResponse(status=404)
