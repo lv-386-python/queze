392418116e4131b6a8e1497ba1029abf1ae20972
Andriy <skyba0andriy@gmail.com>
2019-03-13 14:34:48 +0200

    modified user_answer

--- queze/user_answer/models.py
+++ queze/user_answer/models.py
@@ -1,10 +1,15 @@
+import json
 from django.db import IntegrityError
 from django.db import models
+from django.forms import model_to_dict
+from django.http import JsonResponse
+from django.http import HttpResponse
 
 from authentification.models import CustomUser
 from question.models import Question
 from answer.models import Answer
 from results.models import Results
+
 
 class UserAnswer(models.Model):
     'Class with answer for user'
@@ -15,6 +20,7 @@ class UserAnswer(models.Model):
 
     @staticmethod
     def create_user_answer(text, user, answer):
+        'Create answer for user'
         user_answer = UserAnswer()
         user_answer.question = text
         user_answer.user = user
@@ -25,3 +31,27 @@ class UserAnswer(models.Model):
             return user_answer
         except (ValueError, IntegrityError):
             pass
+
+    @staticmethod
+    def get_user_answer(request, user_answer_id):
+        'Get answer for user'
+        try:
+            user_answer = UserAnswer.object.get(user_answer_id)
+        except Exception:
+            return HttpResponse(status=404)
+        return JsonResponse(model_to_dict(user_answer))
+
+    @staticmethod
+    def update_user_answer(request, user_answer_id):
+        'Update answer for user'
+        try:
+            user_answer = UserAnswer.object.get(id=user_answer_id)
+        except Exception:
+            return HttpResponse(status=404)
+        data = json.loads(request.body)
+        user_answer.question = data["question"]
+        try:
+            user_answer.save()
+            return JsonResponse({'message': 'User answer was successfully updated.'}, status=204)
+        except Exception:
+            return HttpResponse(status=404)
--- queze/user_answer/views.py
+++ queze/user_answer/views.py
@@ -8,32 +8,31 @@ from user_answer.models import UserAnswer
 
 @require_http_methods(["POST"])
 def create_user_answer(request):
+    'Create answer for user'
     data = json.loads(request.body)
     user = request.user
     test = request.test
-    user_answer = UserAnswer.create_quest(data["question"], user, test)
+    user_answer = UserAnswer.create_user_answer(data["question"], user, test)
     if user_answer:
         return HttpResponse(status=201)
     return  HttpResponse(status=404)
 
 @require_http_methods(["GET"])
 def get_user_answer(request, user_answer_id):
-    try:
-        user_answer = UserAnswer.object.get(user_answer_id)
-    except Exception:
+    'Get answer for user'
+    user_answer = UserAnswer.object.get(user_answer_id)
+    if user_answer:
         return HttpResponse(status=404)
     return JsonResponse(model_to_dict(user_answer))
 
 @require_http_methods(["PUT"])
 def update_user_answer(request, user_answer_id):
-    try:
-        user_answer = UserAnswer.object.get(id=user_answer_id)
-    except Exception:
+    'Update answer for user'
+    user_answer = UserAnswer.object.get(id=user_answer_id)
+    if not user_answer:
         return HttpResponse(status=404)
     data = json.loads(request.body)
-    user_answer.question = data["question"]
-    try:
-        user_answer.save()
+    user_answer.update_user_answer = data["question"]
+    if user_answer:
         return JsonResponse({'message': 'User answer was successfully updated.'}, status=204)
-    except Exception:
-        return HttpResponse(status=404)
+    return HttpResponse(status=404)
