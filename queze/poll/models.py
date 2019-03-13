1c1d1133a63166a528c52911357b434da5a53f50
shado1997 <shado20120@gmail.com>
2019-03-13 10:38:04 +0200

    first fix

--- queze/poll/models.py
+++ queze/poll/models.py
@@ -1,85 +1,19 @@
+import json
 from django.db import IntegrityError
 from django.db import models
+from django.http import HttpResponse, JsonResponse
+from django.forms.models import model_to_dict
 from django.utils import timezone
 
-from authentification.models import CustomUser
+from authentification/models.py import CustomUser
 
-
-# Create your models here.
-class Test(models.Model):
-    'Model for Test'
-    # id field is generated authomatically by Django
-    test_name = models.CharField(max_length=35)
-    test_description = models.CharField(max_length=140)
-    test_author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-
-    @staticmethod
-    def create(name, description, user):
-        test = Test()
-        test.test_name = name
-        test.test_description = description
-        test.test_author = user
-        try:
-            test.save()
-            return test
-        except (ValueError, IntegrityError):
-            pass
-
-    @staticmethod
-    def update_test(test_instance, name, description, user):
-        test = test_instance
-        test.test_name = name
-        test.test_description = description
-        test.test_author = user
-        try:
-            test.save()
-            return test
-        except (ValueError, IntegrityError):
-            pass
-
-
-    @staticmethod
-    def delete_test(test_id):
-        test = Test.get_by_id(test_id)
-        if test:
-            test.delete()
-            return True
-        return False
-
-
-    @staticmethod
-    def get_by_id(test_id):
-        try:
-            test = Test.objects.get(id=test_id)
-        except Test.DoesNotExist:
-            print('Not found')
-            return False
-        return test
-
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200, default=1)
-    question_author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=1)
-    question_test = models.ForeignKey(Test, on_delete=models.CASCADE, default=1)
-
-    @staticmethod
-    def create_quest(text, user, key):
-        question = Question()
-        question.question_text = text
-        question.question_author = user
-        question.question_test = key
-        try:
-            question.save()
-            return question
-        except (ValueError, IntegrityError):
-            pass
 
 
 class Answer(models.Model):
     'Asnwers for Questions'
 
     question_id = models.ForeignKey(Question, on_delete=models.CASCADE, default=1)
-    answer_text = models.CharField(max_length=100, default=1)
+    answer_text = models.TextField(default=1)
     answer_auther = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     is_correct = models.BooleanField(default=False)
 
@@ -99,52 +33,46 @@ class Answer(models.Model):
             pass
 
 
-class Results(models.Model):
-    'Class with results of the test'
-    score = models.IntegerField(default=0)
-    result_test = models.ForeignKey(Test, on_delete=models.CASCADE, default=1)
-    result_passer = models.ForeignKey(CustomUser, on_delete=models.CASCADE, default=1)
-    completion_date = models.DateTimeField(auto_now_add=True)
-
     @staticmethod
-    def generate_result(test_id, question_id, user_id):
-        results = Results()
-        results.score = 0
-        results.result_test = test_id
-        results.result_passer = user_id
-        # getting all questions from required test
-        questions = Question().objects.filter(pk=test_id)
-        # getting all correct answers
-        for q in questions:
-            if Answer().objects.filter(pk=q.question_id).filter(is_correct=True):
-                results.score += 1
+    def get_answer(request, answer_id):
         try:
-            results.save()
-            return results
-        except (ValueError, IntegrityError):
-            pass
-
-    @staticmethod
-    def calculate_result(user, test):
-        result = Results.objects.filter(result_test = test, result_passer = user)[0]
-        user_answer = result.user_answers
+            answer = Answer.objects.get(pk=answer_id)
+        except Exception:
+            return HttpResponse(status=404)
+        return JsonResponse(model_to_dict(answer))
 
 
-class UserAnswer(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
-    result = models.ForeignKey(Results, on_delete=models.CASCADE, related_name='user_answers')
-
-    @staticmethod
-    def create_user_answer(text, user, answer):
-        user_answer = UserAnswer()
-        user_answer.question = text
-        user_answer.user = user
-        user_answer.answer = answer
-
+    @@staticmethod
+    def update_answer(request, answer_id):
         try:
-            user_answer.save()
-            return user_answer
-        except (ValueError, IntegrityError):
-            pass
+            answer = Answer.objects.get(pk=answer_id)
+        except Exception:
+            return HttpResponse(status=404)
+        data = json.loads(request.body)
+        answer.answer_text = data['answer_text']
+        try:
+            answer.save()
+            return JsonResponse({'message': 'Answer was successfully updated.'}, status=204)
+        except Exception:
+            return HttpResponse(status=400)
+
+
+    @staticmethod
+    def delete_answer(answer_id):
+        answer = Answer.get_by_id(answer_id)
+        if answer:
+            answer.delete()
+            return True
+        return False
+
+
+    @staticmethod
+    def get_by_id(answer_id):
+        try:
+            answer = Answer.objects.get(id=answer_id)
+        except Answer.DoesNotExist:
+            print('Not found')
+            return False
+        return answer
+
+
--- queze/poll/views.py
+++ queze/poll/views.py
@@ -4,90 +4,9 @@ from django.forms.models import model_to_dict
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.http import require_http_methods
 
-from poll.models import Test, Question, Answer, Results, UserAnswer
+from poll.models import Answer
 
 
-@require_http_methods(["POST"])
-def create_test(request):
-    data = json.loads(request.body)
-    user = request.user
-    test = Test.create(data['test_name'], data['test_description'], user)
-    if test:
-        return HttpResponse(status=201)
-    return HttpResponse(status=400)
-
-
-@require_http_methods(["GET"])
-def get_test(request, test_id):
-    test = Test.get_by_id(test_id)
-    if not test:
-        return HttpResponse(status=404)
-    return JsonResponse(model_to_dict(test))
-
-
-@require_http_methods(["PUT"])
-def update_test(request, test_id):
-    test = Test.get_by_id(test_id)
-    if not test:
-        return HttpResponse(status=404)
-    data = json.loads(request.body)
-    test.update_test(test, data['test_name'], data['test_description'], request.user)
-    if test:
-        return HttpResponse("Successfully updated", status=204)
-    return HttpResponse(status=400)
-
-
-@require_http_methods(["DELETE"])
-def delete_test(request, test_id):
-    is_deleted = Test.delete_test(test_id)
-    if is_deleted:
-        return HttpResponse('Deleted.', status=204)
-    return HttpResponse(status=404)
-
-
-@require_http_methods(["POST"])
-def create_question(request):
-    data = json.loads(request.body)
-    user = request.user
-    test = request.test
-    question = Question.create_quest(data['question_text'], user, test)
-    if question:
-        return HttpResponse(status=201)
-    return HttpResponse(status=400)
-
-
-@require_http_methods(["GET"])
-def get_question(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Exception:
-        return HttpResponse(status=404)
-    return JsonResponse(model_to_dict(question))
-
-
-@require_http_methods(["PUT"])
-def update_question(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Exception:
-        return HttpResponse(status=404)
-    data = json.loads(request.body)
-    question.question_text = data['question_text']
-    try:
-        question.save()
-        return JsonResponse({'message': 'Question was successfully updated.'}, status=204)
-    except Exception:
-        HttpResponse(status=400)
-
-
-@require_http_methods(["DELETE"])
-def delete_question(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Exception:
-        return HttpResponse(status=404)
-    question.delete()
-    return JsonResponse({'message': 'Question was successfully deleted.'}, status=204)
 
 
 @require_http_methods(["POST"])
@@ -103,74 +22,31 @@ def create_answer(request):
 
 @require_http_methods(["GET"])
 def get_answer(request, answer_id):
-    try:
-        answer = Answer.objects.get(pk=answer_id)
-    except Exception:
+    answer = Answer.get_by_id(answer_id)
+    if not answer:
         return HttpResponse(status=404)
     return JsonResponse(model_to_dict(answer))
 
 
 @require_http_methods(["PUT"])
 def update_answer(request, answer_id):
-    try:
-        answer = Answer.objects.get(pk=answer_id)
-    except Exception:
+    answer = Answer.get_by_id(answer_id)
+    if not answer:
         return HttpResponse(status=404)
     data = json.loads(request.body)
-    answer.answer_text = data['answer_text']
-    try:
-        answer.save()
-        return JsonResponse({'message': 'Answer was successfully updated.'}, status=204)
-    except Exception:
-        return HttpResponse(status=400)
+    answer.update_answer = data['answer_text']
+    if answer:
+        return HttpResponse('Answer was successfully updated', status=204)
+    return HttpResponse(status=400)
 
 
 @require_http_methods(["DELETE"])
 def delete_answer(request, answer_id):
-    try:
-        answer = Answer.objects.get(pk=answer_id)
-    except Exception:
-        return HttpResponse(status=404)
-    answer.delete()
-    return JsonResponse({'message': 'Answer was successfully deleted.'}, status=204)
+    is_deleted = Answer.delete_answer(answer_id)
+    if is_deleted:
+        return HttpResponse('Deleted', status=204)
+    return HttpResponse(status=404)
 
 
-@require_http_methods(["GET"])
-def get_results(request, test_id):
-    try:
-        result = Results.objects.format(result_test=test_id)
-    except Exception:
-        return HttpResponse(status=404)
-    return JsonResponse(model_to_dict(result))
 
-@require_http_methods(["POST"])
-def create_user_answer(request):
-    data = json.loads(request.body)
-    user = request.user
-    test = request.test
-    user_answer = UserAnswer.create_quest(data["question"], user, test)
-    if user_answer:
-        return HttpResponse(status=201)
-    return  HttpResponse(status=404)
 
-@require_http_methods(["GET"])
-def get_user_answer(request, user_answer_id):
-    try:
-        user_answer = UserAnswer.object.get(user_answer_id)
-    except Exception:
-        return HttpResponse(status=404)
-    return JsonResponse(model_to_dict(user_answer))
-
-@require_http_methods(["PUT"])
-def update_user_answer(request, user_answer_id):
-    try:
-        user_answer = UserAnswer.object.get(id=user_answer_id)
-    except Exception:
-        return HttpResponse(status=404)
-    data = json.loads(request.body)
-    user_answer.question = data["question"]
-    try:
-        user_answer.save()
-        return JsonResponse({'message': 'User answer was successfully updated.'}, status=204)
-    except Exception:
-        return HttpResponse(status=404)
